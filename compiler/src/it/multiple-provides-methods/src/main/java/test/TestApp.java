f66db3301f3ab496428bbc66e919bb1bb7f89936
Christian Edward Gruber <cgruber@google.com>
2013-01-08 20:09:12 -0500

    Bump and add copyright notices.

--- compiler/src/it/multiple-provides-methods/src/main/java/test/TestApp.java
+++ compiler/src/it/multiple-provides-methods/src/main/java/test/TestApp.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2012 Square, Inc.
+ * Copyright (C) 2013 Square, Inc.
+ * Copyright (C) 2013 Google, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
