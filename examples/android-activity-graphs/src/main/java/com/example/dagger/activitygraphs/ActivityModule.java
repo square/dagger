d36c2af87662cfb205e6f4dc400fd127f691d4f1
Raphael Ackermann <raphael.ackermann@gmail.com>
2014-03-09 01:28:01 +0100

    fixed typo in javadoc

--- examples/android-activity-graphs/src/main/java/com/example/dagger/activitygraphs/ActivityModule.java
+++ examples/android-activity-graphs/src/main/java/com/example/dagger/activitygraphs/ActivityModule.java
@@ -25,7 +25,7 @@ import javax.inject.Singleton;
 
 /**
  * This module represents objects which exist only for the scope of a single activity. We can
- * safely create singletons using the activity instance because ths entire object graph will only
+ * safely create singletons using the activity instance because the entire object graph will only
  * ever exist inside of that activity.
  */
 @Module(
