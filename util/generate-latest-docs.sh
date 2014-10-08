ddc0e8879cf69cb8ffe08d16ca70470716ab36ab
Christian Edward Gruber <cgruber@google.com>
2014-10-08 16:57:17 -0700

    Make javadoc production aggregate, exclude the examples, and avoid an extra jar/unzip process.

--- util/generate-latest-docs.sh
+++ util/generate-latest-docs.sh
@@ -5,17 +5,16 @@ if [ "$TRAVIS_REPO_SLUG" == "google/dagger" ] && \
    [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
    [ "$TRAVIS_BRANCH" == "master" ]; then
   echo -e "Publishing javadoc...\n"
-  mvn javadoc:jar
+  mvn javadoc:aggregate -P!examples
 
   cd $HOME
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "travis-ci"
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/google/dagger gh-pages > /dev/null
   
   cd gh-pages
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
   git rm -rf api/latest 
-  mkdir -p api/latest
-  unzip ${HOME}/core/target/*-SNAPSHOT-javadoc.jar -d api/latest
+  mv ${HOME}/target/site/apidocs api/latest
   git add -f api/latest
   git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
   git push -fq origin gh-pages > /dev/null
