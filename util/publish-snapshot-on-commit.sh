d63256913ea982a18ff2c447c589bf5271c676fd
Christian Edward Gruber <cgruber@google.com>
2014-10-08 15:57:37 -0700

    Use an in-branch settings.xml instead of a weird extra branch.

--- util/publish-snapshot-on-commit.sh
+++ util/publish-snapshot-on-commit.sh
@@ -7,9 +7,8 @@ if [ "$TRAVIS_REPO_SLUG" == "google/dagger" -a \
   echo -e "Publishing maven snapshot...\n"
 
   cd ${HOME}
-  git clone --quiet --branch=travis https://github.com/google/dagger travis > /dev/null
   
-  mvn clean deploy --settings="$HOME/travis/settings.xml" -DskipTests=true -Dinvoker.skip=true -Dmaven.javadoc.skip=true
+  mvn clean deploy --settings="$HOME/util/settings.xml" -DskipTests=true -Dinvoker.skip=true -Dmaven.javadoc.skip=true
 
   echo -e "Published maven snapshot"
 fi
