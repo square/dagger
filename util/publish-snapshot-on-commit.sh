6f0102099189075d917612d365814364bae851e8
Christian Edward Gruber <cgruber@google.com>
2014-10-08 23:43:38 -0700

    Restore original logic in the bash if[] block.

--- util/publish-snapshot-on-commit.sh
+++ util/publish-snapshot-on-commit.sh
@@ -1,14 +1,12 @@
 # see https://coderwall.com/p/9b_lfq
 
-if [ "$TRAVIS_REPO_SLUG" == "google/dagger" -a \
-   "$TRAVIS_JDK_VERSION" == "oraclejdk7" -a \
-   "$TRAVIS_PULL_REQUEST" == "false" -a \
-   "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_REPO_SLUG" == "google/dagger" ] && \
+   [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] && \
+   [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
+   [ "$TRAVIS_BRANCH" == "master" ]; then
   echo -e "Publishing maven snapshot...\n"
 
-  cd ${HOME}
-  
-  mvn clean deploy --settings="$HOME/util/settings.xml" -DskipTests=true -Dinvoker.skip=true -Dmaven.javadoc.skip=true
+  mvn clean deploy --settings="util/settings.xml" -DskipTests=true -Dinvoker.skip=true -Dmaven.javadoc.skip=true
 
   echo -e "Published maven snapshot"
 fi
