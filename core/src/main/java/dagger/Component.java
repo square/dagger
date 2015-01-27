e33a27ce659b3785fc633b777159a662842574d5
dweis <dweis@google.com>
2015-01-26 18:17:50 -0800

    Make `@Component` a runtime-retained annotation, which can allow more interoperability with Guice. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=84836495

--- core/src/main/java/dagger/Component.java
+++ core/src/main/java/dagger/Component.java
@@ -16,12 +16,14 @@
 package dagger;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 import javax.inject.Inject;
 import javax.inject.Provider;
 import javax.inject.Qualifier;
 
 import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * Annotates an interface or abstract class for which a fully-formed, dependency-injected
@@ -55,6 +57,7 @@ import static java.lang.annotation.ElementType.TYPE;
  */
 // TODO(gak): add missing spec for @Scope
 // TODO(gak): add missing spec for component dependencies
+@Retention(RUNTIME) // Allows runtimes to have specialized behavior interoperating with Dagger.
 @Target(TYPE)
 @Documented
 public @interface Component {
