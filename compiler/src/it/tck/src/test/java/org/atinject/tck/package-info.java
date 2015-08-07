c9c47ef83b3aa5ef07115b9e876ab1ad6b6738d8
tonybe <tonybe@google.com>
2015-08-07 14:27:53 -0700

    Remove JSR-330 TCK tests from dagger as we'll run the TCK directly against the published jar.
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=100158048

--- compiler/src/it/tck/src/test/java/org/atinject/tck/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * The compatibility test suite for
- * <a href="http://code.google.com/p/atinject/">JSR-330: Dependency Injection
- * for Java</a>.
- *
- * @see org.atinject.tck.Tck
- */
-package org.atinject.tck;
