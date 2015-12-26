1a0662d8eca7b6e09ea809758af93862fc1cdbfd
Jake Wharton <jw@squareup.com>
2015-12-25 19:02:51 -0500

    Remove redundant interface declarations.
    A Binder of T is a Provider of T and a MembersInjector of T already.

--- compiler/src/main/java/dagger/internal/codegen/InjectAdapterProcessor.java
+++ compiler/src/main/java/dagger/internal/codegen/InjectAdapterProcessor.java
@@ -22,7 +22,6 @@ import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.ParameterizedTypeName;
 import com.squareup.javapoet.TypeSpec;
-import dagger.MembersInjector;
 import dagger.ObjectGraph;
 import dagger.internal.Binding;
 import dagger.internal.Linker;
@@ -38,7 +37,6 @@ import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.inject.Inject;
-import javax.inject.Provider;
 import javax.inject.Singleton;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
@@ -262,15 +260,6 @@ public final class InjectAdapterProcessor extends AbstractProcessor {
         .superclass(ParameterizedTypeName.get(ClassName.get(Binding.class), injectedClassName))
         .addJavadoc("$L", bindingTypeDocs(injectableType(type.asType()), isAbstract,
             injectMembers, dependent).toString());
-
-    if (constructor != null) {
-      result.addSuperinterface(ParameterizedTypeName.get(
-          ClassName.get(Provider.class), injectedClassName));
-    }
-    if (injectMembers) {
-      result.addSuperinterface(ParameterizedTypeName.get(
-          ClassName.get(MembersInjector.class), injectedClassName));
-    }
 
     for (Element field : fields) {
       result.addField(memberBindingField(disambiguateFields, field));
