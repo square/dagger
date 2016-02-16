8ba8668c1c96fc6027904e463b4b501f5feb6664
gak <gak@google.com>
2016-02-16 14:04:58 -0800

    Document nullability for @Provides methods. ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=114791737

--- core/src/main/java/dagger/Provides.java
+++ core/src/main/java/dagger/Provides.java
@@ -25,9 +25,22 @@ import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
- * Annotates methods of a module to create a provider method binding. The
- * method's return type is bound to its returned value. The object graph will
- * pass dependencies to the method as parameters.
+ * Annotates methods of a {@linkplain Module module} to create a provider method binding. The
+ * method's return type is bound to its returned value. The {@linkplain Component component}
+ * implementation will pass dependencies to the method as parameters.
+ *
+ * <h3>Nullability
+ *
+ * <p>Dagger forbids injecting {@code null} by default. Component implemenations that invoke
+ * {@code @Provides} methods that return {@code null} will throw a {@link NullPointerException}
+ * immediately thereafter. {@code @Provides} methods may opt into allowing {@code null} by
+ * annotating the method with any {@code @Nullable} annotation like
+ * {@code javax.annotation.Nullable} or {@code android.support.annotation.Nullable}.
+ *
+ * <p>If a {@code @Provides} method is marked {@code @Nullable}, Dagger will <em>only</em>
+ * allow injection into sites that are marked {@code @Nullable} as well. A component that
+ * attempts to pair a {@code @Nullable} provision with a non-{@code @Nullable} injection site
+ * will fail to compile.
  *
  * @author Bob Lee
  */
