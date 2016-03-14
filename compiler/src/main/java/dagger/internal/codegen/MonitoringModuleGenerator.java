2fb31c0c94feef018cf3aaeb5f38ae6ea99db7dd
beder <beder@google.com>
2016-03-14 13:27:19 -0700

    Clean up the generated MonitoringModule:
    - replace the MonitorCache with @ProductionScope
    - replace the default set binding with @Multibindings
    
    This makes all methods on this module static, so the module instance can be elided.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=117163904

--- compiler/src/main/java/dagger/internal/codegen/MonitoringModuleGenerator.java
+++ compiler/src/main/java/dagger/internal/codegen/MonitoringModuleGenerator.java
@@ -16,16 +16,16 @@
 package dagger.internal.codegen;
 
 import com.google.common.base.Optional;
-import com.google.common.collect.ImmutableSet;
 import com.squareup.javapoet.AnnotationSpec;
 import com.squareup.javapoet.ClassName;
-import com.squareup.javapoet.FieldSpec;
+import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.TypeSpec;
 import dagger.Module;
+import dagger.Multibindings;
 import dagger.Provides;
+import dagger.producers.ProductionScope;
 import dagger.producers.monitoring.ProductionComponentMonitor;
-import dagger.producers.monitoring.internal.MonitorCache;
-
+import dagger.producers.monitoring.internal.Monitors;
 import javax.annotation.processing.Filer;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
@@ -33,13 +33,12 @@ import javax.lang.model.util.Elements;
 
 import static com.squareup.javapoet.MethodSpec.methodBuilder;
 import static com.squareup.javapoet.TypeSpec.classBuilder;
-import static dagger.internal.codegen.AnnotationSpecs.PROVIDES_SET_VALUES;
 import static dagger.internal.codegen.TypeNames.SET_OF_FACTORIES;
 import static dagger.internal.codegen.TypeNames.providerOf;
-import static javax.lang.model.element.Modifier.PRIVATE;
+import static javax.lang.model.element.Modifier.ABSTRACT;
+import static javax.lang.model.element.Modifier.FINAL;
 import static javax.lang.model.element.Modifier.PUBLIC;
 import static javax.lang.model.element.Modifier.STATIC;
-import static javax.lang.model.element.Modifier.FINAL;
 
 /** Generates a monitoring module for use with production components. */
 final class MonitoringModuleGenerator extends JavaPoetSourceFileGenerator<TypeElement> {
@@ -65,27 +64,26 @@ final class MonitoringModuleGenerator extends JavaPoetSourceFileGenerator<TypeEl
             .addAnnotation(
                 AnnotationSpec.builder(Module.class)
                     .build())
-            .addModifiers(PUBLIC, FINAL)
-
-            // TODO(beder): Replace this default set binding with EmptyCollections when it exists.
-            .addMethod(
-                methodBuilder("defaultSetOfFactories")
-                    .returns(SET_OF_FACTORIES)
-                    .addModifiers(STATIC)
-                    .addAnnotation(PROVIDES_SET_VALUES)
-                    .addStatement("return $T.of()", ClassName.get(ImmutableSet.class))
-                    .build())
-            .addField(
-                FieldSpec.builder(MonitorCache.class, "monitorCache", PRIVATE, FINAL)
-                    .initializer("new $T()", MonitorCache.class)
+            .addModifiers(FINAL)
+            .addType(
+                TypeSpec.interfaceBuilder("DefaultSetOfFactories")
+                    .addAnnotation(Multibindings.class)
+                    .addMethod(
+                        MethodSpec.methodBuilder("setOfFactories")
+                            .addModifiers(PUBLIC, ABSTRACT)
+                            .returns(SET_OF_FACTORIES)
+                            .build())
                     .build())
             .addMethod(
                 methodBuilder("monitor")
                     .returns(ProductionComponentMonitor.class)
+                    .addModifiers(STATIC)
                     .addAnnotation(Provides.class)
+                    .addAnnotation(ProductionScope.class)
                     .addParameter(providerOf(ClassName.get(componentElement.asType())), "component")
                     .addParameter(providerOf(SET_OF_FACTORIES), "factories")
-                    .addStatement("return monitorCache.monitor(component, factories)")
+                    .addStatement(
+                        "return $T.createMonitorForComponent(component, factories)", Monitors.class)
                     .build()));
   }
 }
