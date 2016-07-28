46c8e9a6c9c0954dde5012678ca14241875829de
ronshapiro <ronshapiro@google.com>
2016-07-28 07:35:18 -0700

    Modify snapshots to stick to HEAD-SNAPSHOT
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=128697544

--- util/mvn-deploy.sh
+++ util/mvn-deploy.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
-if [ $# -lt 1 ]; then
-  echo "usage $0 <ssl-key> [<param> ...]"
+if [ $# -lt 2 ]; then
+  echo "usage $0 <ssl-key> <version-name> [<param> ...]"
   exit 1;
 fi
 key=$1
-shift
+version_name=$2
+shift 2
+
+if [[ ! $version_name =~ ^2\. ]]; then
+  echo 'Version name must begin with "2."'
+  exit 2
+fi
+
+# Rename snapshot version and sanity check that it doesn't exist in any poms
+sed -i s/HEAD-SNAPSHOT/$version_name/g `find . -name pom.xml`
+if [[ $(git grep HEAD-SNAPSHOT -- '*pom.xml') ]]; then
+  echo "Snapshots found in poms!"
+  exit 3
+fi
 
 #validate key
 keystatus=$(gpg --list-keys | grep ${key} | awk '{print $1}')
