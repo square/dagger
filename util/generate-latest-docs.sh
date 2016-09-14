48ff5a374d265816c22662eececdbb77b6d6d967
ronshapiro <ronshapiro@google.com>
2016-09-14 07:39:10 -0700

    Travis should release our javadoc on jdk8 builds now
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=133125538

--- util/generate-latest-docs.sh
+++ util/generate-latest-docs.sh
@@ -1,7 +1,7 @@
 # see http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/ for details
 
 if [ "$TRAVIS_REPO_SLUG" == "google/dagger" ] && \
-   [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] && \
+   [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && \
    [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
    [ "$TRAVIS_BRANCH" == "master" ]; then
   echo -e "Publishing javadoc...\n"
@@ -10,11 +10,11 @@ if [ "$TRAVIS_REPO_SLUG" == "google/dagger" ] && \
 
   cd $HOME
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/google/dagger gh-pages > /dev/null
-  
+
   cd gh-pages
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
-  git rm -rf api/latest 
+  git rm -rf api/latest
   mkdir -p api
   mv ${TARGET}/site/apidocs api/latest
   git add -f api/latest
