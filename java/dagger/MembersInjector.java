769747afef9e915f87d80ead3afd72aae1565c1e
ronshapiro <ronshapiro@google.com>
2017-05-01 16:09:30 -0700

    Replace "object graph" with "component" in MembersInjector docs
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=154774500

--- java/dagger/MembersInjector.java
+++ java/dagger/MembersInjector.java
@@ -33,9 +33,9 @@ public interface MembersInjector<T> {
    * Injects dependencies into the fields and methods of {@code instance}. Ignores the presence or
    * absence of an injectable constructor.
    *
-   * <p>Whenever the object graph creates an instance, it performs this injection automatically
-   * (after first performing constructor injection), so if you're able to let the object graph
-   * create all your objects for you, you'll never need to use this method.
+   * <p>Whenever a {@link Component} creates an instance, it performs this injection automatically
+   * (after first performing constructor injection), so if you're able to let the component create
+   * all your objects for you, you'll never need to use this method.
    *
    * @param instance into which members are to be injected
    * @throws NullPointerException if {@code instance} is {@code null}
