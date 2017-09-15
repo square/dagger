1679f0a5bd43bda13e64088b223ae13e7cc8e6ce
xaethos <xaethos@google.com>
2017-09-14 18:36:45 -0700

    Add @Nullable annotation to onCreate parameters.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=168779398

--- java/dagger/android/DaggerActivity.java
+++ java/dagger/android/DaggerActivity.java
@@ -19,6 +19,7 @@ package dagger.android;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import dagger.internal.Beta;
 import javax.inject.Inject;
 
@@ -32,7 +33,7 @@ public abstract class DaggerActivity extends Activity implements HasFragmentInje
   @Inject DispatchingAndroidInjector<Fragment> fragmentInjector;
 
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
     AndroidInjection.inject(this);
     super.onCreate(savedInstanceState);
   }
