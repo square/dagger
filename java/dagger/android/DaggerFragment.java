a1bd0eabfca1e6537d8447acaa3de0feb1bd8e32
ronshapiro <ronshapiro@google.com>
2018-06-04 09:42:46 -0700

    Deprecate framework DaggerFragments
    https://github.com/google/dagger/issues/1194
    
    RELNOTES=Deprecated `dagger.android.DaggerFragment` in favor of `dagger.android.support.DaggerFragment` to match Android P's deprecation of framework fragments.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=199146141

--- java/dagger/android/DaggerFragment.java
+++ java/dagger/android/DaggerFragment.java
@@ -25,8 +25,13 @@ import javax.inject.Inject;
  * A {@link Fragment} that injects its members in {@link #onAttach(Context)} and can be used to
  * inject child {@link Fragment}s attached to it. Note that when this fragment gets reattached, its
  * members will be injected again.
+ *
+ * @deprecated Framework fragments are deprecated in Android P; prefer {@code
+ *     dagger.android.support.DaggerFragment} to use a support-library-friendly {@code
+ *     dagger.android} fragment implementation.
  */
 @Beta
+@Deprecated
 public abstract class DaggerFragment extends Fragment implements HasFragmentInjector {
 
   @Inject DispatchingAndroidInjector<Fragment> childFragmentInjector;
