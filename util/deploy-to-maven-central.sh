39586b44fb46b734b9b2dc9e3098faff76b08945
Thomas Broyer <t.broyer@ltgt.net>
2018-07-16 08:37:20 -0700

    Do not create a (useless) branch when releasing
    Closes https://github.com/google/dagger/pull/1204
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=204746929

--- util/deploy-to-maven-central.sh
+++ util/deploy-to-maven-central.sh
@@ -50,13 +50,14 @@ for generated_pom_file in dagger*pom.xml; do
   rm "${generated_pom_file}.asc"
 done
 
-release_branch="release_${version_name}"
-git checkout -b "${release_branch}"
+git checkout --detach
 # Set the version string that is used as a tag in all of our libraries. If another repo depends on
 # a versioned tag of Dagger, their java_library.tags should match the versioned release.
 sed -i s/'${project.version}'/"${version_name}"/g tools/maven.bzl
-git commit -m "${version_name}"
-git push origin "${release_branch}"
+git commit -m "${version_name} release" tools/maven.bzl
 
 git tag -a -m "Dagger ${version_name}" dagger-"${version_name}"
 git push origin tag dagger-"${version_name}"
+
+# Switch back to the original HEAD
+git checkout -
