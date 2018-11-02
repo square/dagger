9934094cdc705dd686f14d4495d9ef1652964d2d
ronshapiro <ronshapiro@google.com>
2018-11-02 10:00:28 -0700

    Extract a ProducerEntryPointView class so other binding expressions that expose producers can use the same logic
    RELNOTES=n/a
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=219815532

--- java/dagger/internal/codegen/ProducerNodeInstanceBindingExpression.java
+++ java/dagger/internal/codegen/ProducerNodeInstanceBindingExpression.java
@@ -17,18 +17,10 @@
 package dagger.internal.codegen;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static dagger.internal.codegen.ComponentImplementation.FieldSpecKind.FRAMEWORK_FIELD;
-import static javax.lang.model.element.Modifier.PRIVATE;
 
 import com.squareup.javapoet.ClassName;
-import com.squareup.javapoet.CodeBlock;
-import com.squareup.javapoet.FieldSpec;
-import com.squareup.javapoet.TypeName;
 import dagger.internal.codegen.ComponentDescriptor.ComponentMethodDescriptor;
 import dagger.model.Key;
-import dagger.producers.Producer;
-import dagger.producers.internal.Producers;
-import javax.lang.model.type.TypeMirror;
 
 /** Binding expression for producer node instances. */
 final class ProducerNodeInstanceBindingExpression extends FrameworkInstanceBindingExpression {
@@ -36,7 +28,7 @@ final class ProducerNodeInstanceBindingExpression extends FrameworkInstanceBindi
   private final ComponentImplementation componentImplementation;
 
   private final Key key;
-  private final TypeMirror type;
+  private final ProducerEntryPointView producerEntryPointView;
 
   ProducerNodeInstanceBindingExpression(
       ResolvedBindings resolvedBindings,
@@ -47,7 +39,7 @@ final class ProducerNodeInstanceBindingExpression extends FrameworkInstanceBindi
     super(resolvedBindings, frameworkInstanceSupplier, types, elements);
     this.componentImplementation = checkNotNull(componentImplementation);
     this.key = resolvedBindings.key();
-    this.type = types.wrapType(resolvedBindings.key().type(), Producer.class);
+    this.producerEntryPointView = new ProducerEntryPointView(types);
   }
 
   @Override
@@ -65,40 +57,9 @@ final class ProducerNodeInstanceBindingExpression extends FrameworkInstanceBindi
   @Override
   Expression getDependencyExpressionForComponentMethod(
       ComponentMethodDescriptor componentMethod, ComponentImplementation component) {
-    if (component.componentDescriptor().kind().isProducer()) {
-      return Expression.create(type, "$N", createField(componentMethod, component));
-    } else {
-      // If the component isn't a production component, it won't implement CancellationListener and
-      // as such we can't create an entry point. But this binding must also just be a Producer from
-      // Provider anyway in that case, so there shouldn't be an issue.
-      // TODO(b/116855531): Is it really intended that a non-production component can have Producer
-      // entry points?
-      return super.getDependencyExpressionForComponentMethod(componentMethod, component);
-    }
-  }
-
-  private FieldSpec createField(
-      ComponentMethodDescriptor componentMethod, ComponentImplementation component) {
-    // TODO(cgdecker): Use a FrameworkFieldInitializer for this?
-    // Though I don't think we need the once-only behavior of that, since I think
-    // getComponentMethodImplementation will only be called once anyway
-    String methodName = componentMethod.methodElement().getSimpleName().toString();
-    FieldSpec field =
-        FieldSpec.builder(
-                TypeName.get(type),
-                component.getUniqueFieldName(methodName + "EntryPoint"),
-                PRIVATE)
-            .build();
-    component.addField(FRAMEWORK_FIELD, field);
-
-    CodeBlock fieldInitialization =
-        CodeBlock.of(
-            "this.$N = $T.entryPointViewOf($L, this);",
-            field,
-            Producers.class,
-            getDependencyExpression(component.name()).codeBlock());
-    component.addInitialization(fieldInitialization);
-
-    return field;
+    return producerEntryPointView
+        .getProducerEntryPointField(this, componentMethod, component)
+        .orElseGet(
+            () -> super.getDependencyExpressionForComponentMethod(componentMethod, component));
   }
 }
