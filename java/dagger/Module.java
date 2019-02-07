2e2d3276101fc7aaf0cf295e140890fc81c1ae27
ronshapiro <ronshapiro@google.com>
2019-02-07 10:39:44 -0800

    Make @Module @Documented
    @ProducerModule is already @Documented
    
    RELNOTES=n/a
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=232902222

--- java/dagger/Module.java
+++ java/dagger/Module.java
@@ -17,6 +17,7 @@
 package dagger;
 
 import dagger.internal.Beta;
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -25,6 +26,7 @@ import java.lang.annotation.Target;
 /**
  * Annotates a class that contributes to the object graph.
  */
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Module {
