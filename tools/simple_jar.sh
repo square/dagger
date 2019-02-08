c366dc2f0f4eb05dec4928bb63f6efed7a4f0bea
dpb <dpb@google.com>
2019-02-08 10:06:46 -0800

    Add unit test for simple_jar.sh.
    Have to make functions to do so.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=233079642

--- tools/simple_jar.sh
+++ tools/simple_jar.sh
@@ -13,29 +13,46 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-readonly PACKAGE_NAME="$1"
-readonly OUT="${PWD}/$2"
-shift 2
+set -e
 
-dirname=""
-for src in "$@"; do
-  if [[ ! "${src}" =~ ^((.*/)?"${PACKAGE_NAME}")/ ]]; then
-    echo "Sources must be in ${PACKAGE_NAME}: $@"
-    exit 1
-  fi
-  src_dirname="${BASH_REMATCH[1]}"
+find_dirname() {
+  local package_name="$1"
+  shift
+
+  local src src_dirname dirname=""
+  for src in "$@"; do
+    if [[ ! "${src}" =~ ^((.*/)?"${package_name}")/ ]]; then
+      echo "Sources must be in ${package_name}: $0" >&2
+      return 1
+    fi
+    src_dirname="${BASH_REMATCH[1]}"
+    if [[ -z "${dirname}" ]]; then
+      dirname="${src_dirname}"
+    elif [[ "${dirname}" != "${src_dirname}" ]]; then
+      echo "Sources must all be in the same directory: $@" >&2
+      return 1
+    fi
+  done
+
   if [[ -z "${dirname}" ]]; then
-    dirname="${src_dirname}"
-  elif [[ "${dirname}" != "${src_dirname}" ]]; then
-    echo "Sources must all be in the same directory: $@"
-    exit 1
+    echo "No sources provided" >&2
+    return 1
   fi
-done
 
-if [[ -z "${dirname}" ]]; then
-  echo "No sources provided"
-  exit 1
+  echo "${dirname}"
+}
+
+main() {
+  local package_name="$1"
+  local out="${PWD}/$2"
+  shift 2
+
+  local dirname
+  dirname="$(find_dirname "${package_name}" "$@")"
+  cd "${dirname}"
+  zip "${out}" -r * &> /dev/null
+}
+
+if [[ -z "$TEST_SRCDIR" ]]; then
+  main "$@"
 fi
-
-cd "${dirname}"
-zip "${OUT}" -r * &> /dev/null
