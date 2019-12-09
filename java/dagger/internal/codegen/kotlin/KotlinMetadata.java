167d9d2587914d368ed6aa6a1b255d24945fd328
danysantiago <danysantiago@google.com>
2019-12-09 08:28:46 -0800

    Throw if metadata is missing when calling getSynthethicPropertyAnnotations.
    RELNOTES=n/a
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=284558322

--- java/dagger/internal/codegen/kotlin/KotlinMetadata.java
+++ java/dagger/internal/codegen/kotlin/KotlinMetadata.java
@@ -163,17 +163,13 @@ final class KotlinMetadata {
   }
 
   /** Parse Kotlin class metadata from a given type element * */
-  static Optional<KotlinMetadata> from(TypeElement typeElement) {
-    return metadataOf(typeElement)
-        .map(
-            metadata -> {
-              MetadataVisitor visitor = new MetadataVisitor();
-              metadata.accept(visitor);
-              return new KotlinMetadata(typeElement, visitor.classFlags, visitor.classProperties);
-            });
+  static KotlinMetadata from(TypeElement typeElement) {
+    MetadataVisitor visitor = new MetadataVisitor();
+    metadataOf(typeElement).accept(visitor);
+    return new KotlinMetadata(typeElement, visitor.classFlags, visitor.classProperties);
   }
 
-  private static Optional<KotlinClassMetadata.Class> metadataOf(TypeElement typeElement) {
+  private static KotlinClassMetadata.Class metadataOf(TypeElement typeElement) {
     Optional<AnnotationMirror> metadataAnnotation =
         getAnnotationMirror(typeElement, Metadata.class);
     Preconditions.checkState(metadataAnnotation.isPresent());
@@ -190,14 +186,14 @@ final class KotlinMetadata {
     KotlinClassMetadata metadata = KotlinClassMetadata.read(header);
     if (metadata == null) {
       // Should only happen on Kotlin < 1.0 (i.e. metadata version < 1.1)
-      return Optional.empty();
+      throw new IllegalStateException(
+          "Unsupported metadata version. Check that your Kotlin version is >= 1.0");
     }
     if (metadata instanceof KotlinClassMetadata.Class) {
       // TODO(user): If when we need other types of metadata then move to right method.
-      return Optional.of((KotlinClassMetadata.Class) metadata);
+      return (KotlinClassMetadata.Class) metadata;
     } else {
-      // Unsupported
-      return Optional.empty();
+      throw new IllegalStateException("Unsupported metadata type: " + metadata);
     }
   }
 
